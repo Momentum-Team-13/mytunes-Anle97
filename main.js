728b2258d6151a49c6babf11ac44f16b2c17093b
An <an.tle.914@gmail.com>
2022-06-07 14:55:16 -0400

    working website without css styling

--- main.js
+++ main.js
@@ -1,50 +1,69 @@
-const searchBox = document.querySelector("#search")
-const searchButton = document.querySelector("#searchbutton")
-const results = document.querySelector(".container")
+const form = document.querySelector("#form")
+const searchBox = document.querySelector("#searchbox")
+const results = document.querySelector("#results")
 const profileElement = document.querySelector(".profile")
 const audioBox = document.querySelector(".audio")
 
-searchButton.addEventListener("click", search)
+form.addEventListener("submit", search)
 
+// grabbing song url from fetch request and assigning it to audio source
 function playSong(url) {
-    console.log(url)
+    audioBox.src = url
+    setTimeout(function() {
+        audioBox.play()
+    }, 1000)
 }
 
 function search() {
     results.innerHTML = "";
     const searchInput = searchBox.value;
 
-    fetch(`https://itunes.apple.com/search?term='${searchInput}`)
+    fetch(`https://itunes.apple.com/search?term='${searchInput}&entity=song`)
         .then((response) => response.json())
         .then((data) => {
         console.log(data)  
-
         for (let song of data.results) {
+            if (data.resultCount === 0) {
+                let noResultsElement = document.createElement('div')
+                noResultsElement.classList.add(".noResults")
+                noResultsElement.innerText = "Sorry, no results. Please search again!"
+                results.appendChild(noResultsElement)
+            } else {
             //profile container and function to grab preview URL
-            let profileElement = document.createElement('div')
-            profileElement.classList.add("profile")
-            profileElement.addEventListener("click", (e) => {
-                playSong(song.previewUrl)
-                })
-            //song name
-            let trackNameElement = document.createElement('div')
-            trackNameElement.classList.add(".trackname")
-            trackNameElement.innerText = song.trackName
-            profileElement.appendChild(trackNameElement)
+                let profileElement = document.createElement('div')
+                profileElement.classList.add("profile")
+                profileElement.addEventListener("click", (e) => {
+                    playSong(song.previewUrl)
+                    })
+                //song name
+                let trackNameElement = document.createElement('div')
+                trackNameElement.classList.add(".trackname")
+                trackNameElement.innerText = song.trackName
+                profileElement.appendChild(trackNameElement)
 
-            //artist name
-            let artistNameElement = document.createElement('div')
-            artistNameElement.classList.add(".artistname")
-            artistNameElement.innerText = song.artistName
-            profileElement.appendChild(artistNameElement)
+                //artist name
+                let artistNameElement = document.createElement('div')
+                artistNameElement.classList.add(".artistname")
+                artistNameElement.innerText = song.artistName
+                profileElement.appendChild(artistNameElement)
 
-            //artwork
-            let artworkElement = document.createElement('img')
-            artworkElement.src = song.artworkUrl60
-            profileElement.appendChild(artworkElement)
+                //artwork
+                let artworkElement = document.createElement('img')
+                artworkElement.src = song.artworkUrl60
+                profileElement.appendChild(artworkElement)
 
-            results.appendChild(profileElement)
+                //album name
+                let albumElement = document.createElement('div')
+                albumElement.classList.add(".album")
+                albumElement.innerText = song.collectionName
+                profileElement.appendChild(albumElement)
+
+                results.appendChild(profileElement)                
+                }            
             }
+        })
+        .catch(err => {
+            alert("Server side error. Please try again!");
         })
 }
 
